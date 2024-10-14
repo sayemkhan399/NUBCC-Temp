6d0ac5a5253d23fca16d36299c8ebce40d3f8f96
Md. Sayem Khan <148582513+sayemkhan399@users.noreply.github.com>
2024-10-14 20:09:28 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    const header = document.getElementById("header");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+  
